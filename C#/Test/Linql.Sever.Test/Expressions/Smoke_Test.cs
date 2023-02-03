cad68299a53ce693e57ef9c17bd098e7a2613b29
TheKrisSodroski <sodroski@bu.edu>
2023-02-02 21:17:12 -0500

    Smoke tests pass

--- C#/Test/Linql.Sever.Test/Expressions/Smoke_Test.cs
+++ C#/Test/Linql.Sever.Test/Expressions/Smoke_Test.cs
@@ -162,211 +162,48 @@ namespace Linql.Server.Test.Expressions
             this.TestFile(nameof(ObjectCalculationWithNull), 0);
         }
 
-        //[Test]
-        //public async Task SimpleConstant()
-        //{
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => true).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.SimpleConstant), simpleConstant);
-        //}
-
-        //[Test]
-        //public async Task SimpleBooleanProperty()
-        //{
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => r.Boolean).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.SimpleBooleanProperty), simpleConstant);
-        //}
-
-        //[Test]
-        //public async Task BooleanNegate()
-        //{
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => !r.Boolean).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.BooleanNegate), simpleConstant);
-        //}
-
-        //[Test]
-        //public async Task SimpleBooleanPropertyChaining()
-        //{
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => r.OneToOne.Boolean).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.SimpleBooleanPropertyChaining), simpleConstant);
-        //}
-
-        //[Test]
-        //public async Task SimpleBooleanPropertyEquals()
-        //{
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => r.Boolean == false).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.SimpleBooleanPropertyEquals), simpleConstant);
-        //}
-
-        //[Test]
-        //public async Task SimpleBooleanPropertyEqualsSwap()
-        //{
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => false == r.Boolean).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.SimpleBooleanPropertyEqualsSwap), simpleConstant);
-        //}
-
-        //[Test]
-        //public async Task TwoBooleans()
-        //{
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => false == true).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.TwoBooleans), simpleConstant);
-        //}
-
-        //[Test]
-        //public async Task BooleanVar()
-        //{
-        //    bool test = false;
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => false == test).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.BooleanVar), simpleConstant);
-        //}
-
-        //[Test]
-        //public async Task ComplexBoolean()
-        //{
-        //    DataModel test = new DataModel();
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => test.Boolean).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.ComplexBoolean), simpleConstant);
-        //}
-
-        //[Test]
-        //public async Task ComplexBooleanAsArgument()
-        //{
-        //    DataModel test = new DataModel();
-        //    await InternalComplexBooleanAsArgument(test);
-
-        //}
-
-        //private async Task InternalComplexBooleanAsArgument(DataModel test)
-        //{
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => test.Boolean).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.ComplexBoolean), simpleConstant);
-        //}
-
-        //[Test]
-        //public async Task ThreeBooleans()
-        //{
-        //    DataModel test = new DataModel();
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => r.Boolean && r.Boolean && r.Boolean).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.ThreeBooleans), simpleConstant);
-        //}
-
-        //[Test]
-        //public async Task ListInt()
-        //{
-        //    List<int> integers = new List<int>() { 1, 2, 3 };
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => integers.Contains(r.Integer)).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.ListInt), simpleConstant);
-        //}
+        [Test]
+        public void ObjectCalculationWithoutNull()
+        {
+            this.TestFile(nameof(ObjectCalculationWithoutNull), 0);
+        }
 
 
-        //[Test]
-        //public async Task ListIntFromProperty()
-        //{
-        //    List<int> integers = new List<int>() { 1, 2, 3 };
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => r.ListInteger.Contains(1)).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.ListIntFromProperty), simpleConstant);
-        //}
+        [Test]
+        public void SimpleBooleanFalse()
+        {
+            this.TestFile(nameof(SimpleBooleanFalse), 0);
+        }
 
-        //[Test]
-        //public async Task InnerLambda()
-        //{
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => r.ListInteger.Any(s => s == 1)).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.InnerLambda), simpleConstant);
-        //}
+        [Test]
+        public void SimpleBooleanProperty()
+        {
+            this.TestFile(nameof(SimpleBooleanProperty), 50);
+        }
 
-        //[Test]
-        //public async Task NullableHasValue()
-        //{
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => r.OneToOneNullable.Integer.HasValue).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.NullableHasValue), simpleConstant);
-        //}
+        [Test]
+        public void SimpleBooleanPropertyChaining()
+        {
+            this.TestFile(nameof(SimpleBooleanPropertyChaining), 50);
+        }
 
-        //[Test]
-        //public async Task NullableValue()
-        //{
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => r.OneToOneNullable.Integer.HasValue && r.OneToOneNullable.Integer.Value == 1).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.NullableValue), simpleConstant);
-        //}
+        [Test]
+        public void SimpleBooleanPropertyEquals()
+        {
+            this.TestFile(nameof(SimpleBooleanPropertyEquals), 50);
+        }
 
-        //[Test]
-        //public async Task LinqlObject()
-        //{
-        //    LinqlObject<DataModel> objectTest = new LinqlObject<DataModel>(new DataModel());
-        //    Assert.That(objectTest.TypedValue, Is.EqualTo(objectTest.Value));
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => objectTest.TypedValue.Integer == r.Integer).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.LinqlObject), simpleConstant);
-        //}
+        [Test]
+        public void SimpleBooleanPropertyEqualsSwap()
+        {
+            this.TestFile(nameof(SimpleBooleanPropertyEqualsSwap), 50);
+        }
 
-        //[Test]
-        //public async Task ObjectCalculationWithNull()
-        //{
-        //    DataModel objectTest = new DataModel();
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => objectTest.OneToOne.Integer == r.Integer).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.ObjectCalculationWithNull), simpleConstant);
-        //}
-
-        //[Test]
-        //public async Task ObjectCalculationWithoutNull()
-        //{
-        //    DataModel objectTest = new DataModel();
-        //    objectTest.OneToOne = new DataModel();
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string simpleConstant = await search.Where(r => objectTest.OneToOne.Integer == r.Integer).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.ObjectCalculationWithoutNull), simpleConstant);
-        //}
-
-        //[Test]
-        //public async Task List_Int_Contains()
-        //{
-        //    List<int> intList = new List<int>() { 1, 2, 3 };
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string contains = await search.Where(r => intList.Contains(r.Integer)).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.List_Int_Contains), contains);
-        //}
-
-        //[Test]
-        //public async Task String_Contains()
-        //{
-
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string contains = await search.Where(r => "3".ToLowerInvariant().Contains(r.String)).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.String_Contains), contains);
-        //}
-
-        //[Test]
-        //public async Task List_Int_Count()
-        //{
-        //    List<int> intList = new List<int>() { 1, 2, 3 };
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string contains = await search.Where(r => intList.Count == 1).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.List_Int_Count), contains);
-        //}
-
-        //[Test]
-        //public async Task Inner_Lambda()
-        //{
-
-        //    LinqlSearch<DataModel> search = Context.Set<DataModel>();
-        //    string contains = await search.Where(r => r.ListRecusrive.Any(s => s.ListInteger.Contains(1))).ToJsonAsync();
-        //    TestLoader.Compare(nameof(Smoke_Test.Inner_Lambda), contains);
-        //}
+        [Test]
+        public void String_Contains()
+        {
+            this.TestFile(nameof(String_Contains), 0);
+        }
     }
 
 }
