c290466b9197070472e98f8728a8a45f4f2b5aca
TheKrisSodroski <sodroski@bu.edu>
2023-02-08 22:42:40 -0500

    more examples

--- C#/ClientExample/Program.cs
+++ C#/ClientExample/Program.cs
@@ -8,16 +8,16 @@ LinqlContext context = new CustomLinqlContext("https://localhost:7113");
 
 LinqlSearch<State> state = context.Set<State>();
 
-// State firstState = await state.FirstOrDefaultAsync();
+State firstState = await state.FirstOrDefaultAsync();
 
-// Console.WriteLine(firstState.State_Name);
+Console.WriteLine(firstState.State_Name);
 
-// List<State> foundStates = await state.Where(r => r.State_Name.ToLower().Contains("ma")).ToListAsync();
+List<State> foundStates = await state.Where(r => r.State_Name.ToLower().Contains("ma")).ToListAsync();
 
-// foundStates.ForEach(r =>
-// {
-//     Console.WriteLine($"{r.State_Name}");
-// });
+foundStates.ForEach(r =>
+{
+    Console.WriteLine($"{r.State_Name}");
+});
 
 Point point = new Point(new Coordinate(-77.036530, 38.897675));
 LinqlObject<Point> linqlPoint = new LinqlObject<Point>(point);
