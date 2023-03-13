90a8616deed6bdd5f37c496fcd7c68bc5d4df715
TheKrisSodroski <sodroski@bu.edu>
2023-03-13 18:32:19 -0400

    updates

--- Typescript/scripts/updateVersion.js
+++ Typescript/scripts/updateVersion.js
@@ -1,7 +1,26 @@
-const { execSync } = require('child_process')
-const library = process.argv.pop();
+const {
+    globSync,
+} = require('glob')
 
-const path = `projects/${ library }`;
-execSync(`npm i`, { stdio: 'inherit' });
-execSync(`npm i`, { stdio: 'inherit', cwd: path });
-execSync(`npm run build ${ library }`, { stdio: 'inherit' });
+const fs = require('fs');
+
+const ignore = [
+    'node_modules/**',
+    "examples/**"
+]
+
+const version = process.argv.pop();
+
+const packages = globSync('**/package.json', { ignore: ignore });
+
+console.log(packages);
+
+
+packages.forEach(r =>
+{
+    const file = fs.readFileSync(r, 'utf-8');
+    const parsed = JSON.parse(file);
+    parsed.version = version;
+    const updatedFile = JSON.stringify(parsed, null, 2);
+    fs.writeFileSync(r, updatedFile);
+});
