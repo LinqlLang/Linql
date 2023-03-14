b16e212aace4295527aa9ce9dec45e6415be048e
TheKrisSodroski <sodroski@bu.edu>
2023-03-14 18:50:52 -0400

    new script

---
+++ Typescript/scripts/installAll.js
@@ -0,0 +1,24 @@
+const { execSync } = require('child_process')
+
+const {
+    globSync,
+} = require('glob')
+
+const fs = require('fs');
+
+const ignore = [
+    'node_modules/**',
+    "examples/**"
+]
+
+const packages = globSync('**/package.json', { ignore: ignore });
+
+console.log(packages);
+
+packages.forEach(r =>
+{
+    const file = fs.readFileSync(r, 'utf-8');
+    const parsed = JSON.parse(file);
+    execSync(`npm run install ${ parsed.name }`, { stdio: 'inherit' });
+});
+
