b2383db47f8c3a0db844c74fdb96725410b98aad
TheKrisSodroski <sodroski@bu.edu>
2023-05-07 18:27:32 -0400

    have async functions working.  Have EF6 tests working

--- C#/Test/Linql.Sever.EF6.Test/BuildingTest.cs
+++ C#/Test/Linql.Sever.EF6.Test/BuildingTest.cs
@@ -1,12 +1,19 @@
+using Linql.Client;
+using Linql.Core;
+using Linql.Server.EF6.Test.DataModel;
 using Linql.Test.Files;
+using System.Data.Entity;
+using System.Reflection;
 
 namespace Linql.Server.EF6.Test
 {
     public class BuildingTest : TestFileTests
     {
         bool ResetDatabase { get; set; } = false;
-        EF6TestContext Context { get; set; } 
-      
+        EF6TestContext Context { get; set; }
+
+        public LinqlCompiler Compiler { get; set; }
+
         [OneTimeSetUp]
         public override async Task Setup()
         {
@@ -23,25 +30,33 @@ namespace Linql.Server.EF6.Test
 
             //Data = dataList.AsQueryable();
 
-            //HashSet<Assembly> assemblies = new HashSet<Assembly>()
-            //{
-            //    typeof(Boolean).Assembly,
-            //    typeof(Enumerable).Assembly,
-            //    typeof(Queryable).Assembly
-            //};
-
-            //HashSet<Assembly> extensionCompiler = new HashSet<Assembly>(assemblies);
-            //extensionCompiler.Add(typeof(EF6Test).Assembly);
-
-            //this.Compiler = new LinqlCompiler(assemblies);
-            //this.ExtensionCompiler = new LinqlCompiler(extensionCompiler);
+            HashSet<Assembly> assemblies = new HashSet<Assembly>()
+            {
+                typeof(Boolean).Assembly,
+                typeof(Enumerable).Assembly,
+                typeof(Queryable).Assembly,
+                typeof(DbContext).Assembly
+            };
 
 
+            this.Compiler = new LinqlCompiler(assemblies);
+          
         }
 
         [Test]
-        public void WhereFalse()
+        public void Take10()
         {
+            Assert.DoesNotThrow(() =>
+            {
+                int take = 10;
+                LinqlSearch<Building> search = new LinqlSearch<Building>();
+                LinqlSearch compiledSearch = search.Take(take).ToListAsyncSearch();
+
+                List<Building> data = this.Compiler.Execute<List<Building>>(compiledSearch, this.Context.Buildings);
+
+                Assert.That(data.Count(), Is.EqualTo(take));
+
+            });
         }
 
       
