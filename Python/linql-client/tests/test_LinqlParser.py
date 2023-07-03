86f8cb6a7eae4e410d70d6c5477353250ef10edc
TheKrisSodroski <sodroski@bu.edu>
2023-07-03 11:58:09 -0400

    more tests

--- Python/linql-client/tests/test_LinqlParser.py
+++ Python/linql-client/tests/test_LinqlParser.py
@@ -4,9 +4,11 @@ from src.linql_client.LinqlParser import LinqlParser
 from src.linql_client.LinqlContext import LinqlContext
 from src.linql_client.LinqlSearch import LinqlSearch
 from .FileLoader import FileLoader
+from typing import Self
 
 class DataModel:
-   Boolean: bool 
+   Boolean: bool
+   OneToOne: Self 
 
 context = LinqlContext(LinqlSearch, "")
 testLoader = FileLoader("../C#/Test/Linql.Test.Files/TestFiles/Smoke")
@@ -30,4 +32,9 @@ class TestLinqlParser:
    def test_BooleanNegate(self):
       search: LinqlSearch[DataModel] = context.Set(DataModel)
       newSearch = search.Where(lambda r: not r.Boolean)
+      testLoader.ExecuteTest(newSearch)
+
+   def test_SimpleBooleanPropertyChaining(self):
+      search: LinqlSearch[DataModel] = context.Set(DataModel)
+      newSearch = search.Where(lambda r: r.OneToOne.Boolean)
       testLoader.ExecuteTest(newSearch)
