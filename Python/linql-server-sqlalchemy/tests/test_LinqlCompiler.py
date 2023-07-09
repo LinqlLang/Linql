93c1ca82aaf2d890b8ac0b0de3639102381fc7b8
Kris Sodroski <sodroski@bu.edu>
2023-07-09 18:23:12 -0400

    started building out the sql alchemy compiler

--- Python/linql-server-sqlalchemy/tests/test_LinqlCompiler.py
+++ Python/linql-server-sqlalchemy/tests/test_LinqlCompiler.py
@@ -1,7 +1,24 @@
 from .FileLoader import FileLoader
 from typing import Self
+from src.linql_server_sqlalchemy.LinqlCompiler import LinqlCompiler
+from linql_client.LinqlContext import LinqlContext
+from linql_client.LinqlSearch import LinqlSearch
+import tests.Building as Building
+import json
+import pytest
+
+compiler = LinqlCompiler({
+   Building
+})
+
+context = LinqlContext(LinqlSearch, "")
 
 class TestLinqlCompiler:
 
-   def test_EmptySearch(self):
+   @pytest.mark.asyncio
+   async def test_EmptySearch(self):
+     set: LinqlSearch[Building.Building] = context.Set(Building.Building)
+     emptySearch = set.ToListAsyncSearch().toJson()
+     reloadSearch: LinqlSearch = json.loads(emptySearch)
+     await compiler.ExecuteAsync(reloadSearch, None)
      assert True
